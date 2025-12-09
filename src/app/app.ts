0638c789259bfc37c33ef979419c50e45acd78c5
Devanshi Patel <devanshipatel2824@gmail.com>
2025-12-09 18:34:47 +0530

    Create the componet and create website

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Homepage } from './homepage/homepage';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  //templateUrl: './app.html',
-  //styleUrl: './app.css'
-template:`<h1>Hello World</h1> <h1>Hii Hello</h1>`,
+  imports: [RouterOutlet,Header,Homepage,Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+/*template:`<h1>Hello World</h1> <h1>Hii Hello</h1>`,
 
 styles:
-'h1{ color:red;}'
+'h1{ color:red;}'*/
 })
 export class App {
   protected readonly title = signal('Angular_project');

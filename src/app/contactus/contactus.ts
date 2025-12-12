35d4e2e5aee5e6aa21018cff720d74c1e9a82a7c
Devanshi Patel <devanshipatel2824@gmail.com>
2025-12-12 18:28:04 +0530

    data binding

--- src/app/contactus/contactus.ts
+++ src/app/contactus/contactus.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contactus.html',
   styleUrl: './contactus.css',
 })
 export class Contactus {
+ protected name="Devanshi";
+protected imageurl="/images/avatar.png";
+protected username?:string;
+protected studentname=signal("Devanshi patel");
+
+
 
 }

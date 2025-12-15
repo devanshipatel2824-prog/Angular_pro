e110591d453169d260a376dc13b9c336f74f9fd3
Devanshi Patel <devanshipatel2824@gmail.com>
2025-12-15 22:11:40 +0530

    Add the contect,menu pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { PageNotFound } from './page-not-found/page-not-found';
 import { About } from './about/about';
+import { Menu } from './menu/menu';
+import { HealthyFood } from './menu/healthy-food/healthy-food';
+import { JunkFood } from './menu/junk-food/junk-food';
+import { ItalicFood } from './menu/italic-food/italic-food';
+import { Contact } from './contact/contact';
 
 export const routes: Routes = [
 {
@@ -20,6 +25,28 @@ export const routes: Routes = [
 {
     path:'about',
     component:About
+},
+{
+    path:'menu',
+    component:Menu,
+    children:[
+        {
+            path:'healthy-food',
+            component:HealthyFood
+        },
+        {
+            path:'junk-food',
+            component:JunkFood
+        },
+        {
+            path:'italic-food',
+            component:ItalicFood
+        },
+]
+},
+{
+    path:'contact',
+    component:Contact
 },
 {
     path:"**",
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -11,21 +11,21 @@ protected imageurl1="/image/home_page 1.jpg";
 protected imageurl2="/image/home_page2.jpg";
 protected imageurl3="/image/home_page3.jpg";
   currentIndex = 0;
-  totalSlides = 3;
 
-  nextSlide() {
-    if (this.currentIndex < this.totalSlides - 1) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0; // Loop back to first
-    }
+nextSlide() {
+  if (this.currentIndex < 2) {
+    this.currentIndex++;
+  } else {
+    this.currentIndex = 0;
   }
+}
 
-  prevSlide() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    } else {
-      this.currentIndex = this.totalSlides - 1; // Loop to last
-    }
+prevSlide() {
+  if (this.currentIndex > 0) {
+    this.currentIndex--;
+  } else {
+    this.currentIndex = 2;
   }
+}
+
 }
